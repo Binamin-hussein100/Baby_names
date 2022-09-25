8137a07ed758ee614a81c90eedb1f569e81f80ce
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-09-25 10:04:03 +0300

    added a post space

--- src/homepage.js
+++ src/homepage.js
@@ -1,18 +1,21 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Navbar from './navbar';
 import React,{useState, useEffect} from 'react';
 import Pagination from './pagination';
-
-
+import Button from 'react-bootstrap/Button';
+import Collapse from 'react-bootstrap/Collapse';
+import Search from './search';
+import Form from 'react-bootstrap/Form'
+import { FormGroup } from 'react-bootstrap';
 
 function Homepage(){
   const [names, setNames] = useState([])
-  const [loading, setLoading] = useState(false)
   const [currentPage, setCurrentPage] = useState(1)
   const [postsPerPage, setPostsPerPage] = useState(10)
-
+  // State for collaps
+  const [open, setOpen] = useState(false)
+  
   useEffect(() => {
     fetch('http://localhost:9292/baby_names')
     .then((res) =>res.json())
@@ -27,22 +30,49 @@ const currentNames = names.slice(indexOfFirstPost, indexOfLastPost)
 const paginate = (pageNumbers) => setCurrentPage(pageNumbers)
 
     return (
-      <Container className="homepage">
+      <Container fluid className="homepage">
             <Row>
-                <Col xs lg="10">
-                  <h2>Gone</h2>
+              <Col>
+                  <Search name={names}/>
+              </Col>
+            </Row>
+            <Row>
+                <Col xs lg="8">
                     <ul>
                       {currentNames.map((name) =>(
-                        <li key={name.id}>
-                            <p>{name.name}</p>
+                        <li key={name.id} className='names'>
+                            <Container>
+                                <Row>
+                                    <Col lg="8">
+                                        <p id='single'>{name.name}</p>
+                                        <p>Gender: {name.gender}</p>
+                                    </Col>
+                                    <Col >
+                                       <Button 
+                                          onClick={()=>setOpen(!open)} 
+                                          aria-controls="control" 
+                                          aria-expanded={open} 
+                                          variant='outline-dark'>
+                                            Meaning</Button>
+                                          <Collapse in={open}>
+                                              <div id='control'>
+                                                  <h6 className="each">Origin: {name.origin}</h6>
+                                                  <h6 className='each'>Meaning: {name.meaning}</h6>
+                                              </div>
+                                          </Collapse>
+                                    </Col>
+                                </Row>
+                            </Container>
                         </li>
                       ))}
                     </ul>
                     <Pagination postsPerPage={postsPerPage} totalPosts={names.length} paginate={paginate}/>
                 </Col>
-                <Col>
-                  half done
+                <Col  className='add'>
+                    ADD A NAME
+                    
                 </Col>
+               
             </Row>
       </Container>
     )
