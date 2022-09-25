f482b2a424028ee8c6f97a73c3863325d839cf4e
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-09-25 10:06:41 +0300

    added a search feature

--- src/search.js
+++ src/search.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
 
-function Search(){
+function Search(props){
+    const [search, setSearchVal] = useState('')
+
+    const handleInput = (e) =>{
+        setSearchVal(e.target.value)
+    }
+
+    const handleClearBtn = () =>{
+        setSearchVal('')
+    }
+
+    // const filteredProducts = props.names.filter((name)=>{
+    //         return name.includes(search)
+    // })
+
     return (
         <>
+            <Form>
+                <Form.Group>
+                    <Form.Control type='search' placeholder='Search....'/>
+                </Form.Group>
+                <Button variant='outline-primary'>
+                    Search
+                </Button>
+            </Form>            
         </>
     )
 
