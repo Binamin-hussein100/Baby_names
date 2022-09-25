598679f312c2d95df3943d069c9ab82f61eacbbc
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-09-25 10:05:30 +0300

    imported link from react router dom

--- src/navbar.js
+++ src/navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-
+import {Link} from 'react-router-dom'
+ 
 
 function Navigation() {
     return (
         <Navbar expand="lg" variant="light" bg="light">
         <Container fluid>
-          <Navbar.Brand href="#">Navbar</Navbar.Brand>
+          <Navbar.Brand id='logo' href="#">NameIt</Navbar.Brand>
         </Container>
       </Navbar>
 
