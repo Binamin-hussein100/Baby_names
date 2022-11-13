5961ee3c5783ebe21fdfa6c4679d5a662a9afb38
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-11-13 11:45:25 +0300

    added navbar content

--- src/navbar.js
+++ src/navbar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import {Link} from 'react-router-dom'
- 
+import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav'
 
 function Navigation() {
     return (
-        <Navbar expand="lg" variant="light" bg="dark">
+        <Navbar expand="lg" variant="light" bg="light">
         <Container fluid>
           <Navbar.Brand id='logo' href="#">NameIt</Navbar.Brand>
+        </Container>
+        <Container>
+            <Nav.Link to='/landing'>Home</Nav.Link>
         </Container>
       </Navbar>
 
