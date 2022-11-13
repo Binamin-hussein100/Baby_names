9fdd910e9bdf41ef200245548deff4bc51163a5a
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-11-13 11:43:26 +0300

    imported routers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import Homepage from './homepage';
 import Navigation from './navbar';
+import LandingPage from './landingPage'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
+import {Routes, Route} from "react-router-dom"
 
 
 function App() {
   return (
     <Container fluid className='application'>
         <Navigation/>
-        <Homepage/>
+        <LandingPage/>
+          {/* <Routes>
+              <Route path='/' exact element={<Homepage/>}/>  
+              <Route path='/landing' element={}/> 
+          </Routes> */}
+           
     </Container>
     
 
